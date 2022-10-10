d51722f07c32225515198082f18c94e9c1c1d866
mulenRouge <mulenrouge28@yandex.ru>
2022-10-10 15:41:36 +0300

    D7

---
+++ D3/Program.cs
@@ -0,0 +1,80 @@
+﻿/*
+Напишите программу, которая на вход принимает число и возвращает позицию (i, j) этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Введите число m");
+int n = GetNumber("Введите число n");
+int findNumber = GetNumber("Введите искомое число в массиве случайных чисел");
+int[,] matrix = InitMatrix(m, n);
+
+Console.WriteLine("Матрица:");
+PrintMatrix(matrix);
+bool existNum = false;
+
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (matrix[i, j] == findNumber)
+        {
+            existNum = true;
+        }
+    }
+}
+
+if (existNum != true)
+    Console.WriteLine("Число не в массиве");
+
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (matrix[i, j] == findNumber)
+        {
+            Console.WriteLine($"Искомое число находится на позиции строка: {i + 1} и столбец: {j + 1}");
+        }
+    }
+}
